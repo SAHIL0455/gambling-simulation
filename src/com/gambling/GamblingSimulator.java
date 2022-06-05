7730815330ac33f384c02478d38fbf6bbf07febc
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-05 22:17:11 +0530

    ADDED: Gambler make 1 bet so either win or loss 1 bet

--- src/com/gambling/GamblingSimulator.java
+++ src/com/gambling/GamblingSimulator.java
@@ -5,5 +5,15 @@ public class GamblingSimulator {
         System.out.println("Welcome the Gambling Simulator");
         int everyDayStake = 100;
         int everyGameBet = 1;
+        int bit = (int) Math.floor(Math.random()*10)%2;
+        int win = 1;
+        if (bit == win){
+            System.out.println("Your are Win");
+            everyDayStake++;
+        }else {
+            System.out.println("Your are loss");
+            everyDayStake--;
+        }
+        System.out.println("Now Your stake is = "+everyDayStake);
     }
 }
