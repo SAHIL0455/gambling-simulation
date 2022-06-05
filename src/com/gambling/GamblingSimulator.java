cb27696de4d86d0be4f48c5d0aca3d86e540b159
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-05 23:46:23 +0530

    20 Days of Playing

--- src/com/gambling/GamblingSimulator.java
+++ src/com/gambling/GamblingSimulator.java
@@ -3,24 +3,28 @@ package com.gambling;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulator");
-        int everyDayStake = 100;
+        int everyStake = 100;
         int everyGameBet = 1;
-        while (everyDayStake > 50 && everyDayStake < 150) {
-            int bit = (int) Math.floor(Math.random() * 10) % 2;
-            int win = 1;
-            if (bit == win) {
-                System.out.println("Your are Win");
-                everyDayStake++;
-            } else {
-                System.out.println("Your are loss");
-                everyDayStake--;
+        int days = 21;
+        int win = 1;
+        for (int i = 1; i < days; i++) {
+            int handCash = everyStake;
+            while (handCash > 50 && handCash < 150) {
+                double bit = Math.floor(Math.random() * 10) % 2;
+                if (bit == win) {
+                    System.out.println("Your are Win");
+                    handCash++;
+                } else {
+                    System.out.println("Your are loss");
+                    handCash--;
+                }
+                System.out.println("Now Your stake is = " + handCash);
             }
-            System.out.println("Now Your stake is = " + everyDayStake);
-        }
-        if (everyDayStake == 50){
-            System.out.println("You Have Loss 50% Stake Today");
-        }else {
-            System.out.println("You Have Win 50% Stake Today");
+            if (handCash == 50) {
+                System.out.println("You Have Loss 50% Stake Today");
+            } else {
+                System.out.println("You Have Win 50% Stake Today");
+            }
         }
     }
 }
