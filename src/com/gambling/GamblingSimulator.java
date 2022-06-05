40152257aaf02093af8a36ea51083a777098fc4e
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-05 22:07:53 +0530

    Added: Every Day Stake And Every GAme Bet

--- src/com/gambling/GamblingSimulator.java
+++ src/com/gambling/GamblingSimulator.java
@@ -3,5 +3,7 @@ package com.gambling;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome the Gambling Simulator");
+        int everyDayStake = 100;
+        int everyGameBet = 1;
     }
 }
