16075c47a27d5c4356371f59eba0be29c9678f4e
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-05 22:30:34 +0530

    ADDED: Gambler if won or Loss 50% of The Stake

--- src/com/gambling/GamblingSimulator.java
+++ src/com/gambling/GamblingSimulator.java
@@ -5,15 +5,22 @@ public class GamblingSimulator {
         System.out.println("Welcome the Gambling Simulator");
         int everyDayStake = 100;
         int everyGameBet = 1;
-        int bit = (int) Math.floor(Math.random()*10)%2;
-        int win = 1;
-        if (bit == win){
-            System.out.println("Your are Win");
-            everyDayStake++;
-        }else {
-            System.out.println("Your are loss");
-            everyDayStake--;
+        while (everyDayStake > 50 && everyDayStake < 150) {
+            int bit = (int) Math.floor(Math.random() * 10) % 2;
+            int win = 1;
+            if (bit == win) {
+                System.out.println("Your are Win");
+                everyDayStake++;
+            } else {
+                System.out.println("Your are loss");
+                everyDayStake--;
+            }
+            System.out.println("Now Your stake is = " + everyDayStake);
         }
-        System.out.println("Now Your stake is = "+everyDayStake);
+        if (everyDayStake == 50){
+            System.out.println("You Have Loss 50% Stake Today");
+        }else {
+            System.out.println("You Have Win 50% Stake Today");
+        }
     }
 }
